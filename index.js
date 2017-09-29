95e9f32c85db6eb72219b6ac5b3d059b75c564d6
hashrock <hashedrock@gmail.com>
2017-09-30 02:05:40 +0900

    feat: add frontend

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
-const fs = require("fs")
-const http = require("https")
-const client = require('cheerio-httpcli');
+var express = require('express');
+var app = express();
+const fetchConnpass = require("./lib/parser").fetchConnpass
 
-const url = "https://vuejs-meetup.connpass.com/event/65442/participation/"
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-function retFirstMatched(str, regex){
-  var matched = str.match(regex)
-  if(matched && matched.length > 0){
-    return matched[1]
-  }
-  return ""
-}
-
-client.fetch(url, { }, function (err, $, res, body) {
-  // レスポンスヘッダを参照
-  console.log(res.headers);
-
-  // HTMLタイトルを表示
-  console.log($('title').text());
-
-  var result = []
-  $('.participants_table tr').each(function (idx) {
-    const screenName = $(this).find(".display_name").text().trim()
-    const connpassUrl = $(this).find(".display_name a").attr("href")
-    var connpassId = ""
-    if(connpassUrl){
-      connpassId = retFirstMatched(connpassUrl, /^https:\/\/connpass.com\/user\/([^\/]+)\/$/)
-    }
-    const ptype = $(this).find(".label_ptype_name").text().trim()
-    const status = $(this).find(".label_status_tag").text().trim().replace(/ /g, "").split("\n").join("#")
-    const link = $(this).find(".social a").attr("href")
-    var gh = ""
-    var tw = ""
-    if(link){
-      gh = retFirstMatched(link, /^https:\/\/github.com\/([^\/]+)\/$/)
-      tw = retFirstMatched(link, /^https:\/\/twitter.com\/intent\/user\?screen_name=(.+)$/)
-    }
-    
-    if(screenName){
-      result.push({
-        screenName: screenName,
-        id: connpassId,
-        ptype: ptype,
-        status: status,
-        github: gh,
-        twitter: tw
-      })
-    }
-  });
-
-  console.log(result)
-
+app.get('/connpass', function(request, response) {
+  fetchConnpass((result)=>{
+    response.json(result)
+  })
 });
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
+
